b95207c98e7de47b62663a2e741de145b9a7967c
Abdul4code <abuabdul4dev@gmail.com>
2022-07-26 18:15:08 -0700

    Adding team images

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -11,14 +11,14 @@ st.set_page_config(
 with open('resources/style/home.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
         
-st.markdown("<div class='head'> Meet Strataz </div>", unsafe_allow_html=True)
+st.markdown("<div class='head'> Who we are </div>", unsafe_allow_html=True)
         
 col1, col2 = st.columns([2, 1])
 
 col1.markdown("""<p class='about'> Strataz is an analytics consulting firm that is focused on providing
-               organizations with advanced analytic tools that allows them scale. We boost of competent 
-               teams which are able to deliver cost effective and efficient tools in handling different business problems
-               ranging from marketing to decision making to human mangement etc.</p> """, unsafe_allow_html=True)
+               organizations with advanced analytic tools that allows them scale. We boost of a competent 
+               team which is able to deliver cost effective and efficient tools in handling different business problems
+               ranging from marketing to decision making, human mangement etc.</p> """, unsafe_allow_html=True)
                
 col2.markdown("<h2 class='top_head'> Our Mission <h2>", unsafe_allow_html=True)
 col2.markdown("""<p class='mission'> Our mission is to help organisations optimize the user experience
@@ -27,26 +27,26 @@ col2.markdown("""<p class='mission'> Our mission is to help organisations optimi
 
 team1, team2, team3, team4 = st.columns(4)
 
-team1.image('resources/imgs/img.jpg', use_column_width=True)
+team1.image('resources/imgs/Abdul.jpg', use_column_width=True)
 team1.markdown("<p class='name'> Abubakar Abdulkadir </p>", unsafe_allow_html=True)
 team1.markdown("<p class='role'> Team Lead </p>", unsafe_allow_html=True)
 
-team1.image('resources/imgs/img.jpg', use_column_width=True)
+team1.image('resources/imgs/joseph.jpg', use_column_width=True)
 team1.markdown("<p class='name'> Joseph Okonkwo </p>", unsafe_allow_html=True)
 team1.markdown("<p class='role'> Data Scientist </p>", unsafe_allow_html=True)
 
-team2.image('resources/imgs/img.jpg', use_column_width=True)
+team2.image('resources/imgs/ubasinachi.jpg', use_column_width=True)
 team2.markdown("<p class='name'> Ubasinachi Eleonu</p>", unsafe_allow_html=True)
 team2.markdown("<p class='role'>Data Analyst </p>", unsafe_allow_html=True)
 
-team2.image('resources/imgs/img.jpg', use_column_width=True)
+team2.image('resources/imgs/bongani.jpg', use_column_width=True)
 team2.markdown("<p class='name'> Bongani Mkhizee </p>", unsafe_allow_html=True)
 team2.markdown("<p class='role'> Product Designer </p>", unsafe_allow_html=True)
 
-team3.image('resources/imgs/img.jpg', use_column_width=True)
+team3.image('resources/imgs/mamah.jpg', use_column_width=True)
 team3.markdown("<p class='name'> Micheal Mamah </p>", unsafe_allow_html=True)
 team3.markdown("<p class='role'> Data Scientist </p>", unsafe_allow_html=True)
 
-team4.image('resources/imgs/img.jpg', use_column_width=True)
-team4.markdown("<p class='name'> Abubakar Abdulkadir </p>", unsafe_allow_html=True)
-team4.markdown("<p class='role'> Team Lead </p>", unsafe_allow_html=True)
+team4.image('resources/imgs/marvic.jpg', use_column_width=True)
+team4.markdown("<p class='name'> Micheal Cocourvi </p>", unsafe_allow_html=True)
+team4.markdown("<p class='role'> Business Strategist </p>", unsafe_allow_html=True)
